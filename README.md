d454c6526d86588d1ca848699d46f5fe0c73dbf1
Martin Hansen <zity89@gmail.com>
2021-02-10 08:18:31 +0100

    Populating repository

---
+++ README.md
@@ -0,0 +1,23 @@
+# iptables manager
+This program is a very simple manager for loading custom iptables rules. When the program is started, it will create a set of custom chains, to which all subsequent rules are added. When the program stops, it will flush and delete these custom chains.
+
+## Download
+The manager is written in [Go](https://golang.org) and it can be downloaded and compiled using:
+```bash
+go get github.com/mrlhansen/iptables_manager
+```
+
+## Usage
+The program accepts three arguments. The start and stop options can be added at the same time, which will result in a reload of all rules.
+```
+Usage of iptables_manager:
+-confdir string
+	  path to the configuration directory (default "/etc/iptmgr")
+-start
+	  start the manager
+-stop
+	  stop the manager
+```
+
+## Operation
+When the program starts it will initialize a new set of chains using the rules in `<confdir>/start.rules` and when it stops it will delete these chains by reading the rules in `<confdir>/stop.rules`. Editing these two files might prevent the program from working properly, but they are kept as external files for flexibility. On startup, after initializing the new chains, it will proceed to read and apply the rules from all files in `<confdir>/rules.d` in ordered sequence.
---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"flag"
+	"bytes"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"sort"
+	"log"
+)
+
+func lsdir(path string) []string {
+	var files []string
+
+	list, err := ioutil.ReadDir(path)
+	if err != nil {
+		log.Fatal("unable to read directory: ", path)
+	}
+
+	for _,file := range list {
+		if file.IsDir() == false {
+			files = append(files, path+"/"+file.Name())
+		}
+	}
+
+	sort.Strings(files)
+	return files
+}
+
+func read_rules(filename string, replace bool) []string {
+	var lines []string
+	var text string
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal("unable to read file: ", filename)
+	}
+	text = string(content)
+
+	if replace {
+		text = strings.ReplaceAll(text, "INPUT", "iptmgr-input")
+		text = strings.ReplaceAll(text, "OUTPUT", "iptmgr-output")
+		text = strings.ReplaceAll(text, "FORWARD", "iptmgr-forward")
+		text = strings.ReplaceAll(text, "PREROUTING", "iptmgr-prerouting")
+		text = strings.ReplaceAll(text, "POSTROUTING", "iptmgr-postrouting")
+	}
+
+	lines = strings.Split(text, "\n")
+	for i,line := range lines {
+		line = strings.TrimSpace(line)
+		if len(line) > 0 && line[0] == '#' {
+			line = ""
+		}
+		lines[i] = line
+	}
+
+	return lines
+}
+
+func apply_rules(filename string, rules []string) {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	for i,rule := range rules {
+		if len(rule) == 0 {
+			continue
+		}
+
+		cmd := exec.Command("bash", "-c", "iptables " + rule)
+		cmd.Stdout = &stdout
+		cmd.Stderr = &stderr
+		err := cmd.Run()
+
+		if err != nil {
+			s := strings.Split(stderr.String(), "\n")
+			log.Fatalf("%s:%d %s\n", filename, i, s[0])
+		}
+	}
+}
+
+func manager_start(path string) {
+	file := path + "/start.rules"
+	files := lsdir(path + "/rules.d")
+	rules := read_rules(file, false)
+	apply_rules(file, rules)
+
+	for _,file = range files {
+		rules = read_rules(file, true)
+		apply_rules(file, rules)
+	}
+}
+
+func manager_stop(path string) {
+	file := path + "/stop.rules"
+	rules := read_rules(file, false)
+	apply_rules(file, rules)
+}
+
+func main() {
+	var path string
+	var start *bool
+	var stop *bool
+
+	flag.StringVar(&path, "confdir", "/etc/iptmgr", "path to the configuration directory")
+	start = flag.Bool("start", false, "start the manager")
+	stop = flag.Bool("stop", false, "stop the manager")
+	flag.Parse()
+
+	if *stop {
+		manager_stop(path)
+	}
+
+	if *start {
+		manager_start(path)
+	}
+}
