06302101a793427f72d49c211991b5267f09ad90
Martin Hansen <zity89@gmail.com>
2021-05-10 08:36:00 +0200

    Delete chains if a rule fails

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"bytes"
 	"io/ioutil"
+	"os"
 	"os/exec"
 	"strings"
 	"sort"
@@ -36,8 +37,8 @@ func read_rules(filename string, replace bool) []string {
 	if err != nil {
 		log.Fatal("unable to read file ", filename)
 	} else {
-        log.Print("reading file ", filename)
-    }
+		log.Print("reading file ", filename)
+	}
 	text = string(content)
 
 	if replace {
@@ -60,7 +61,7 @@ func read_rules(filename string, replace bool) []string {
 	return lines
 }
 
-func apply_rules(filename string, rules []string) {
+func apply_rules(filename string, rules []string) bool {
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
 
@@ -76,27 +77,42 @@ func apply_rules(filename string, rules []string) {
 
 		if err != nil {
 			s := strings.Split(stderr.String(), "\n")
-			log.Fatalf("%s:%d %s\n", filename, i, s[0])
+			log.Printf("%s:%d %s\n", filename, i, s[0])
+			return false
 		}
 	}
+
+	return true
 }
 
 func manager_start(path string) {
 	file := path + "/start.rules"
 	files := lsdir(path + "/rules.d")
 	rules := read_rules(file, false)
-	apply_rules(file, rules)
+
+	ok := apply_rules(file, rules)
+	if !ok {
+		os.Exit(1)
+	}
 
 	for _,file = range files {
 		rules = read_rules(file, true)
-		apply_rules(file, rules)
+		ok := apply_rules(file, rules)
+		if !ok {
+			manager_stop(path)
+			os.Exit(1)
+		}
 	}
 }
 
 func manager_stop(path string) {
 	file := path + "/stop.rules"
 	rules := read_rules(file, false)
-	apply_rules(file, rules)
+
+	ok := apply_rules(file, rules)
+	if !ok {
+		os.Exit(1)
+	}
 }
 
 func main() {
